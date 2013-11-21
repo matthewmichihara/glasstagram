ae49f88a1980f851c9340cd030d42dfdf779f128
Matthew Michihara <matthew.michihara@gmail.com>
2013-11-20 21:52:54 -0800

    Include PhotoProcessing as a submodule

---
+++ App/README.md
@@ -0,0 +1,4 @@
+glasstagram
+===========
+
+Take cool pics like ![Alt text](https://pbs.twimg.com/media/BZibk61CQAAS55I.png:large)
---
+++ App/src/com/fourpool/glasstagram/SecretKeys.java
@@ -0,0 +1,8 @@
+package com.fourpool.glasstagram;
+
+public final class SecretKeys {
+	public static final String CONSUMER_KEY = "";
+	public static final String CONSUMER_SECRET = "";
+	public static final String ACCESS_TOKEN = "";
+	public static final String ACCESS_TOKEN_SECRET = "";
+}
